2e5eb8fe8e6ec5698424a4bab9000d1a68d872af
Sam Barratt <milktheelephant@gmail.com>
2016-04-10 09:46:43 +0100

    Fixed typo

--- src/__init__.py
+++ src/__init__.py
@@ -14,20 +14,24 @@ def home():
 
 @app.route('/checkid/<id>', methods=['GET'])
 def checkID(id):    #Takes an ID, finds it in the DB, and checks if it has anthrax
-    database = databaseConnection(settings.dbString,setting.dbPort) #Connect to db
+    database = databaseConnection(settings.dbString,settingsdbPort) #Connect to db
     return checkID(database,id) #check the ID
 
 @app.route('/checkname/<surname>', methods=['GET'])
 def checkName(name):    #Takes a surname, finds it in the DB, and analyzes the person's path from that point. Should return any other people who might have anthrax in the area
-    database = databaseConnection(settings.dbString,setting.dbPort) #Connect to db
+    database = databaseConnection(settings.dbString,settingsdbPort) #Connect to db
     return checkName(database,name)
 
 @app.route('/analyzeid/<id>', methods=['POST'])
 def analyzeID(id):    #Takes an ID, finds it in the DB, and checks if it has anthrax. Should return any other people who might have anthrax in the area
+    database = databaseConnection(settings.dbString,settingsdbPort) #Connect to db
+
     return False
 
 @app.route('/analyzename/<surname>', methods=['POST'])
 def analyzeName(name):    #Takes a surname, finds it in the DB, and checks if it has anthrax
+    database = databaseConnection(settings.dbString,settingsdbPort) #Connect to db
+
     return False
 
 @app.route('/alertStatus/')
