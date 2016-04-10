95dc4d5c359b4488fe5238763caff367a747cc4b
Sam Barratt <milktheelephant@gmail.com>
2016-04-10 09:52:03 +0100

    Fleshed out analyze

--- src/analyze.py
+++ src/analyze.py
@@ -2,12 +2,13 @@
 
 def analyzeID(db, id):  
     """Takes an ID, tracks the movement attached to it and finds other people that may have come in contact""" 
+    patient = db.getpatientdetails(patientid=id)
     return none
 
-def analyzeName(db, id):  
+def analyzeName(db, name):  
    """ Takes an ID, tracks the movement attached to it and finds other people that may have come in contact""" 
+    patient = db.getpatientdetails(patientsurname=name)
     return none
-
 
 def analyze(patiant):
     """ Takes a Patiant and returns the percentage chance that they have anthrax"""
