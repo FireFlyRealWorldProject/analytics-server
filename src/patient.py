e6b583c0153734664f6f9ef0cb5501df33926afd
Sam Barratt <milktheelephant@gmail.com>
2016-04-10 09:49:06 +0100

    Fleshed out getField method

--- src/patient.py
+++ src/patient.py
@@ -6,14 +6,21 @@ class patient:
 
     def __init__(self,jsonPatient):
         """ constructor, takes the dictionary of patient data """
+        self.patientData = jsonPatient
+
         return
 
-    def setLocations(self,locations):
+    def setLocations(self,db,locations):
         """ Adds a locations field to the patient data. Must be coupled manually because we want to keep them seperate really """
-        self.patientData
+        self.patientData['locations'] = db.getPatientLocations(id=self.patientData['id'])
         return 
 
     def getField(self,field):
         """ Returns the field from patientData. We have a specific method for this so we can catch fields that don't exist."""
-        return patientData[field]
+        try:
+            data = patientData[field]
+        except KeyError:
+            return none
+
+        return data
 
